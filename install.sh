3b76765220aaed4eed18d1b25937e845b994fbbe
Samuel Ostholm <sostholm@gmail.com>
2021-11-01 18:05:56 +0100

    adding install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+
+sudo adduser micro-node-exporter --disabled-login
+sudo mkdir /home/micro-node-exporter/micro-node-exporter
+sudo cp app.py /home/micro-node-exporter/micro-node-exporter
+sudo chown micro-node-exporter /home/micro-node-exporter/micro-node-exporter/app.py
+sudo cp micro-node-exporter.service /etc/systemd/service/
+sudo -u micro-node-exporter /usr/bin/python3 -m pip install -r requirements.txt
+sudo systemctl enable micro-node-exporter
+sudo systmectl start micro-node-exporter
